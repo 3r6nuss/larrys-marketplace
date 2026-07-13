48530dbcc985637cd899e0f97f3ab9064abe8945
3r6nuss <steinerfinn09@gmail.com>
2026-07-13 19:24:00 +0200

    feat: remove unused SelectValue import from CatalogPage component

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -6,7 +6,7 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
 import { Input } from '@/components/ui/input';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Select, SelectContent, SelectItem, SelectTrigger } from '@/components/ui/select';
 import { Skeleton } from '@/components/ui/skeleton';
 import {
  Dialog, DialogContent, DialogHeader, DialogTitle,
